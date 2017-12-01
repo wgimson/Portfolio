d645395385e0a9443e4c8c2e683aec7cddbd152a
wgimson <wgimson@mmm.com>
2017-12-01 18:56:53 -0500

    scroll to contact page section added

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,16 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { Router, NavigationEnd, ActivatedRoute } from "@angular/router";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"]
 })
 export class HomeComponent implements OnInit {
+  constructor(public router: Router, private route: ActivatedRoute) {}
 
-  constructor(public router: Router) {
-   }
+  jumpToHero() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+  ngAfterViewInit(): void {
+    this.route.fragment.subscribe(f => {
+      const element = document.querySelector("#" + f);
+      if (element) {
+        element.scrollIntoView(); // <-- omit element from the argument
+      }
+    });
   }
 }
